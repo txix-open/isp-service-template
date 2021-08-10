d84d760d1c18577586115aa32dfffc81d4359a92
Мержанов Максим Владимирович <maksimmerzh@gmail.com>
2021-08-10 11:50:11 +0000

    Update dependencies

--- conf/local_config.go
+++ conf/local_config.go
@@ -1,13 +1,12 @@
 package conf
 
 import (
+	"github.com/integration-system/isp-lib/v2/config"
 	"github.com/integration-system/isp-lib/v2/structure"
 )
 
 type Configuration struct {
-	InstanceUuid         string                         `valid:"required~Required"`
-	ModuleName           string                         `valid:"required~Required"`
-	ConfigServiceAddress structure.AddressConfiguration `valid:"required~Required"`
-	GrpcOuterAddress     structure.AddressConfiguration `valid:"required~Required"`
-	GrpcInnerAddress     structure.AddressConfiguration `valid:"required~Required"`
+	config.CommonLocalConfig
+	GrpcOuterAddress structure.AddressConfiguration `valid:"required~Required"`
+	GrpcInnerAddress structure.AddressConfiguration `valid:"required~Required"`
 }
