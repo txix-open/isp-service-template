3319b9837f2bc6098800c0b82e9c8b980a3e7046
d1slike <merser1q@gmail.com>
2021-12-14 17:12:25 +0300

    MSP-429 add service routes

---
+++ routes/routes.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"github.com/integration-system/isp-kit/cluster"
+	"github.com/integration-system/isp-kit/grpc"
+	"github.com/integration-system/isp-kit/grpc/endpoint"
+	"github.com/integration-system/isp-kit/grpc/isp"
+	"msp-service-template/usecase/object"
+)
+
+type Controllers struct {
+	Object object.Controller
+}
+
+func EndpointDescriptors() []cluster.EndpointDescriptor {
+	return endpointDescriptors(Controllers{})
+}
+
+func Handler(mapper endpoint.Mapper, c Controllers) isp.BackendServiceServer {
+	muxer := grpc.NewMux()
+	for _, descriptor := range endpointDescriptors(c) {
+		muxer.Handle(descriptor.Path, mapper.Endpoint(descriptor.Handler))
+	}
+	return muxer
+}
+
+func endpointDescriptors(c Controllers) []cluster.EndpointDescriptor {
+	return []cluster.EndpointDescriptor{{
+		Path:             "msp-service-template/object/all",
+		Inner:            false,
+		UserAuthRequired: false,
+		Handler:          c.Object.Objects,
+	}, {
+		Path:             "msp-service-template/object/get_by_id",
+		Inner:            false,
+		UserAuthRequired: false,
+		Handler:          c.Object.GetById,
+	}}
+}
