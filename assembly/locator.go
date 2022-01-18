2c49340b509c38825d4a2a5989c9d4eabcca6665
d1slike <merser1q@gmail.com>
2022-01-18 17:58:05 +0300

    MSP-429 build rabbit mq configuration in locator

--- assembly/locator.go
+++ assembly/locator.go
@@ -2,21 +2,29 @@ package assembly
 
 import (
 	"github.com/integration-system/isp-kit/db"
+	"github.com/integration-system/isp-kit/grmqx"
 	"github.com/integration-system/isp-kit/grpc/endpoint"
 	"github.com/integration-system/isp-kit/grpc/isp"
 	"github.com/integration-system/isp-kit/log"
+	"msp-service-template/conf"
 	"msp-service-template/controller"
 	"msp-service-template/repository"
 	"msp-service-template/routes"
 	"msp-service-template/service"
+	"msp-service-template/transaction"
 )
 
+type DB interface {
+	db.DB
+	db.Transactional
+}
+
 type Locator struct {
-	db     db.DB
+	db     DB
 	logger log.Logger
 }
 
-func NewLocator(db db.DB, logger log.Logger) Locator {
+func NewLocator(db DB, logger log.Logger) Locator {
 	return Locator{
 		db:     db,
 		logger: logger,
@@ -33,4 +41,20 @@ func (l Locator) Handler() isp.BackendServiceServer {
 	mapper := endpoint.DefaultWrapper(l.logger)
 	handler := routes.Handler(mapper, c)
 	return handler
+}
+
+func (l Locator) BrokerConfig(consumerCfg conf.Consumer) grmqx.Config {
+	txManager := transaction.NewManager(l.db)
+	msgService := service.NewMessage(l.logger, txManager)
+	msgController := controller.NewMessage(msgService)
+
+	handler := grmqx.NewResultHandler(l.logger, msgController)
+	consumer := consumerCfg.Config.DefaultConsumer(handler, l.logger)
+
+	brokerConfig := grmqx.NewConfig(
+		consumerCfg.Client.Url(),
+		grmqx.WithConsumers(consumer),
+		grmqx.WithDeclarations(grmqx.TopologyFromConsumers(consumerCfg.Config)),
+	)
+	return brokerConfig
 }
